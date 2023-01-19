e850bf327fe99d482892be495b8257e662ac666d
Gavin H <gavinmajetich@gmail.com>
2023-01-19 14:06:58 -0500

    Can now add, remove, and save repo list

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
-use std::process::exit;
-use std::fs::{File, OpenOptions};
-use std::path::Path;
-use std::io::{Write, BufReader, BufRead, Error};
-use requestty::{Question, question::Choice::Choice, Answers, Answer, prompt};
-use subprocess::{Popen, PopenConfig, Redirection};
 use regex::Regex;
+use requestty::{prompt, question::Choice::Choice, Answer, Answers, Question};
+use std::fs::{File, OpenOptions};
+use std::io::{BufRead, BufReader, Error, Write};
+use std::path::Path;
+use std::process::exit;
+use subprocess::{Popen, PopenConfig, Redirection};
 
 fn main() {
     // Check git is installed
@@ -38,7 +38,6 @@ fn main() {
         }
     }
 
-    // TODO
     // First thing's first, need to check if there is a save file with the list of repositories.
     // If there is not, create one. If there is, load them into a vector. This vector will be saved to the file anytime
     // a repository is added to or removed from the list, or on quit.
@@ -46,19 +45,23 @@ fn main() {
         .read(true)
         .write(true)
         .create(true)
-        .open("repos.txt") {
-            Ok(file) => file,
-            Err(_) => { println!("fatal: Could not open or create repository list!"); exit(1); }
-        };
+        .open("repos.txt")
+    {
+        Ok(file) => file,
+        Err(_) => {
+            println!("fatal: Could not open or create repository list at start!");
+            exit(1);
+        }
+    };
 
     // Read all lines from it and add to list of repositories
     let mut repo_list: Vec<String> = Vec::new();
-    let bufreader = BufReader::new(repository_list_file);
+    let bufreader = BufReader::new(repository_list_file.try_clone().unwrap());
     for line in bufreader.lines() {
         match line {
             Ok(text) => {
                 repo_list.push(text);
-            },
+            }
             Err(_) => {
                 println!("fatal: Error reading data from repos.txt file!");
                 exit(1);
@@ -71,19 +74,18 @@ fn main() {
     // Can now proceed to show menu
     // Show the menu indefinitely, let user exit when they want to exit
     println!("Welcome to git-sync!");
-    loop { 
+    loop {
         let main_menu = Question::select("menu")
             .message("Select an action:")
             .choices(vec![
                 "Add a repository".to_string(),
                 "Remove a repository".to_string(),
                 "Sync all repositories".to_string(),
-                "Help".to_string(),
-                "Quit".to_string()
+                "Quit".to_string(),
             ])
             .default(0)
             .build();
-        
+
         let selection = prompt(vec![main_menu]);
         if let Ok(sel) = selection {
             for ans in sel.values() {
@@ -104,7 +106,7 @@ fn main() {
                             let repo_dir_res = prompt(vec![directory_question]);
                             match repo_dir_res {
                                 Ok(answers) => {
-                                    // Add it to the memory-resident list and 
+                                    // Add it to the memory-resident list and
                                     // serialize it to the file
                                     for ans in answers {
                                         let repo = ans.1.as_string().unwrap().to_string();
@@ -112,31 +114,127 @@ fn main() {
                                             repo_list.push(repo.clone());
                                             println!("Successfully added '{}' to sync list.", repo);
                                             // Add to file here.
+                                            let mut repository_list_file = match OpenOptions::new()
+                                                .append(true)
+                                                .open("repos.txt")
+                                            {
+                                                Ok(file) => file,
+                                                Err(_) => {
+                                                    println!("fatal: Could not open or create repository list!");
+                                                    exit(1);
+                                                }
+                                            };
+                                            writeln!(repository_list_file, "{}", repo)
+                                                .expect("fatal: Error writing to file.");
                                         }
                                     }
-                                    
-                                },
-                                Err(_) => { 
+                                }
+                                Err(_) => {
                                     println!("There was an error processing your input...");
                                 }
                             }
-                        },
+                        }
                         // Remove a repository
                         1 => {
-
-                        },
+                            // Get a directory from the prompt
+                            let directory_question = Question::input("add-repo")
+                                .message("Enter path to repository to remove from sync system: ")
+                                .validate(|repo, previous_answers| {
+                                    if Path::new(repo).is_dir()
+                                        && Path::new(&format!("{}\\.git", repo)).is_dir()
+                                    {
+                                        Ok(())
+                                    } else {
+                                        Err("Please enter a valid git repository.".to_string())
+                                    }
+                                })
+                                .build();
+                            let repo_dir_res = prompt(vec![directory_question]);
+                            match repo_dir_res {
+                                Ok(answers) => {
+                                    // Add it to the memory-resident list and
+                                    // serialize it to the file
+                                    for ans in answers {
+                                        let repo = ans.1.as_string().unwrap().to_string();
+                                        if repo_list.contains(&repo) {
+                                            repo_list.remove(
+                                                repo_list
+                                                    .iter()
+                                                    .position(|r| *r == repo.clone())
+                                                    .unwrap(),
+                                            );
+                                            println!("Successfully removed '{}' from sync list. This did NOT remove it from your disk.", repo);
+                                            println!("This change will be serialized when you soft quit the program.")
+                                        } else {
+                                            println!("No such repository found in sync list...");
+                                        }
+                                    }
+                                }
+                                Err(_) => {
+                                    println!("There was an error processing your input...");
+                                }
+                            }
+                        }
                         // Sync all repositories
                         2 => {
-                            
-                        },
-                        // Print help
-                        3 => {},
+                            for repo in repo_list.iter() {
+                                // Check git is installed
+                                let pr = Popen::create(
+                                    &["cmd.exe", "/c", format!("cd {} && git pull", repo).as_str()],
+                                    PopenConfig {
+                                        stdout: Redirection::Pipe,
+                                        ..Default::default()
+                                    },
+                                );
+
+                                match pr {
+                                    Ok(mut pop) => {
+                                        println!("Ran git pull on {}", repo);
+                                        let (out, err) = pop.communicate(None).unwrap();
+                                        println!(
+                                            "Output: {}",
+                                            out.unwrap_or("No output".to_string())
+                                        );
+                                        if let Some(_status) = pop.poll() {
+                                            // Program already exit correctly
+                                        } else {
+                                            // Terminate the program
+                                            pop.terminate().unwrap();
+                                        }
+                                    }
+                                    Err(_e) => {
+                                        println!("Could not open cmd.exe.");
+                                        exit(1);
+                                    }
+                                }
+                            }
+                        }
                         // Exit program
-                        4 => {
-                            exit(0)
-                        },
+                        3 => {
+                            let mut conglomerate_string = String::new();
+                            for repo in repo_list {
+                                conglomerate_string.push_str(repo.as_str());
+                                conglomerate_string.push_str("\n");
+                            }
+
+                            let mut repository_list_file = match OpenOptions::new()
+                                .write(true)
+                                .truncate(true)
+                                .create(true)
+                                .open("repos.txt")
+                            {
+                                Ok(file) => file,
+                                Err(_) => {
+                                    println!("fatal: Could not open or create repository list!");
+                                    exit(1);
+                                }
+                            };
+                            write!(repository_list_file, "{}", conglomerate_string)
+                                .expect("fatal: Could not save to file");
+                            exit(0);
+                        }
                         // This should never happen
-                        _ => { 
+                        _ => {
                             println!("I didn't understand that...");
                         }
                     }
